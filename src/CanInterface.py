5c58d555d342e684b55b803de904d1320aa4863b
KingDeng005 <fuheng.deng@tusimple.ai>
2017-08-30 17:03:15 -0700

    add source code for this package

---
+++ src/CanInterface.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import sys
+import canlib
+import rospy
+
+cl = canlib.canlib()
+
+class CanInterface:
+
+	def __init__(self, can_serial_number=0, can_circuit_id=0):
+		self.channel = None
+		self.ch = None
+		
+		#Get Channel from serial number and circuit id
+		for i in range(cl.getNumberOfChannels()):
+			if(can_serial_number == cl.getChannelData_Serial(i)):
+				self.channel = i + can_circuit_id
+				break
+
+		if(self.channel == None):
+			rospy.logerr("CAN Interface Error: Incorrect serial_number or can_circuit_id.")
+			sys.exit()
+		self.setUp(self.channel)
+
+	def setUp(self, channel):
+		channels = cl.getNumberOfChannels()
+		if self.channel >= channels:
+			rospy.loginfo('Invalid channel number')
+			sys.exit()
+		try:
+			self.ch = cl.openChannel(self.channel, canlib.canOPEN_ACCEPT_VIRTUAL)
+			self.ch.setBusOutputControl(canlib.canDRIVER_NORMAL)
+			self.ch.setBusParams(canlib.canBITRATE_500K, 4, 3, 1, 1, 0)
+			self.ch.busOn()
+		except (canlib.canError) as ex:
+			rospy.logerr("CAN Interface Error: " + str(ex))
+			raise ex
+
+			
---
+++ src/can2ros.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# Created by Fuheng Deng on 08/29/2017
+
+import rospy
+import struct
+import sys
+from can_msgs.msg import Frame
+from ros2can_util.msg import *
+from CanInterface import CanInterface
+from canlib import *
+
+class can2ros:
+	def __init__(self, can_hardware_id=None, can_circuit_id=0):
+		rospy.loginfo('starting caninterface...')
+		try:
+			self.canInterface = CanInterface(can_hardware_id, can_circuit_id)
+			self.pub_can_rx = rospy.Publisher('/can_rx', CanMessageStamped, queue_size=1)
+		except:
+			rospy.logerr('can interface init failed...')
+			sys.exit()
+		self.start()
+
+	def start(self):
+		rospy.logdebug('Start publishing message...')
+		rate = rospy.Rate(20)
+		while not rospy.is_shutdown():
+			try:
+				for i in range(85):
+					#Use canlib to read can data
+					frame = CanMessageStamped()
+					frame.msg = CanMessage()
+					frame.msg.id, msg, frame.msg.dlc, flg, time = self.canInterface.ch.read()
+					rospy.logdebug('hello world')
+					frame.header.stamp = rospy.Time.now()
+					is_error = (flg & canMSG_ERROR_FRAME > 0) # Error Frame Flag
+					frame.msg.extended = (flg & canMSG_EXT > 0) #Extended Frame Flag
+					if not is_error:
+						frame.msg.data = list(msg)
+						self.pub_can_rx.publish(frame)
+					rate.sleep()
+			except (canNoMsg) as ex:
+				pass
+			except (canError) as ex:
+				rospy.logerr("CAN Error: " + str(ex))
+		rospy.spin()
+		
+
+if __name__ == "__main__":
+	rospy.init_node('can2ros', anonymous=True)
+	_ = can2ros(36847,0)
+	rospy.spin()
